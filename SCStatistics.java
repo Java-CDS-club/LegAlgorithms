43c0f41d63cffc2032798f7b1b4a88ea62e6a16f
sasamil <sasa.milenkovic.xyz@gmail.com>
2018-07-02 04:03:33 +0200

    Add files via upload
    First (rough) version.
    
    1) File reading and extracting the needed fields (date, time, speed, heading)
    
    2) Ordinary statistic methods. They are only developed and tested. At the moment, they are still not used in steady_course or steady_speed functions.
    
    3) steady_coarse_periods() and steady_speed_periods() methods. It's just an initial attempt; a very rough determination of 'steady' intervals. (without any statistics applied).
    
    todo: 
    - Applying statistic methods for splitting and/or merging intervals
    - Simultaneous handling of course and speed; overlapping steady course and steady speed intervals.
    - Regression analysis (big expectations)

---
+++ SCStatistics.java
@@ -0,0 +1,291 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * SCStatistics - utility methods of mathematical statistics. Various tools for extracting data patterns 
+ * and making predictions about the vessels' positions.
+ */
+public class SCStatistics {
+	
+    //-------------------------------------------------------------------------
+    /**
+     * Creates a double array of speed values from an ArrayList of totes
+     * @param  totes given ArrayList of Tote objects
+     * @return double[] - array of speeds extracted form totes
+     */
+    public static double[] getSpeeds(ArrayList<Tote> totes) {
+        int index = 0;
+        double[] da = new double[totes.size()];
+        Iterator<Tote> iter = totes.iterator();
+        while (iter.hasNext())
+            da[index++] = iter.next().dspeed;
+
+        return da;
+    }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Creates a double array of speed values from an ArrayList of totes within the given indexes
+     * @param  totes given ArrayList of Tote objects
+     * @param  istart (inclusive) lower index of the totes-array segment extracted for calculation
+     * @param  iend (exclusive) upper index of the totes-array segment extracted for calculation
+     * @return double[] - array of speeds extracted form totes
+     */
+    public static double[] getSpeeds(ArrayList<Tote> totes, int istart, int iend) {
+        if (0 > istart)
+            throw new IndexOutOfBoundsException("Calling ScStatistics.getSpeeds - 'istart' index (" + istart + ") is less than 0.");
+
+        if (totes.size() < iend)
+            throw new IndexOutOfBoundsException("Calling ScStatistics.getSpeeds - 'iend' index (" + iend + ") is greater than array length.");
+        
+        if (istart > iend)
+            throw new NegativeArraySizeException("Calling ScStatistics.getSpeeds - 'istart' (" + istart + ") is less than 'iend' (" + iend + ").");
+        
+        int index = 0;
+        double[] da = new double[iend - istart];
+        for (int ii=istart; ii<iend; ii++)
+            da[index++] = totes.get(ii).dspeed;
+
+        return da;
+    }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Creates a double array of heading values from an ArrayList of totes
+     * @param  totes given ArrayList of Tote objects
+     * @return double[] - array of headings extracted form totes
+     */
+    public static double[] getHeadings(ArrayList<Tote> totes) {
+        double[] dl = new double[totes.size()];
+        int index = 0;
+        Iterator<Tote> iter = totes.iterator();
+        while (iter.hasNext())
+            dl[index++] = iter.next().dheading;
+
+        return dl;
+    }
+	
+    //-------------------------------------------------------------------------
+    /**
+     * Creates a double array of sheading values from an ArrayList of totes within the given indexes
+     * @param  totes given ArrayList of Tote objects
+     * @param  istart (inclusive) lower index of the totes-array segment extracted for calculation
+     * @param  iend (exclusive) upper index of the totes-array segment extracted for calculation
+     * @return double[] - array of headings extracted form totes
+     */
+    public static double[] getHeadings(ArrayList<Tote> totes, int istart, int iend) {
+        if (0 > istart)
+            throw new IndexOutOfBoundsException("Calling ScStatistics.getHeadings - 'istart' index (" + istart + ") is less than 0.");
+
+        if (totes.size() < iend)
+            throw new IndexOutOfBoundsException("Calling ScStatistics.getHeadings - 'iend' index (" + iend + ") is greater than array length.");
+        
+        if (istart > iend)
+            throw new NegativeArraySizeException("Calling ScStatistics.getHeadings - 'istart' (" + istart + ") is less than 'iend' (" + iend + ").");
+        
+        int index = 0;
+        double[] da = new double[iend - istart];
+        for (int ii=istart; ii<iend; ii++)
+            da[index++] = totes.get(ii).dheading;
+
+        return da;
+    }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Returns maximal value from an array of doubles
+     * @param  ardoubles given array
+     * @return double - max value within the array
+     */
+    public static double max(double[] ardoubles) {
+        double dmax = Double.MIN_VALUE;
+        for (int ii=0; ii<ardoubles.length; ii++)
+            dmax = Double.max(dmax, ardoubles[ii]);
+
+        return dmax;
+    }
+	
+    //-------------------------------------------------------------------------
+    /**
+     * Returns minimal value from an array of doubles
+     * @param  ardoubles given array
+     * @return double - min value within the array
+     */
+    public static double min(double[] ardoubles) {
+        double dmin = Double.MAX_VALUE;
+        for (int ii=0; ii<ardoubles.length; ii++)
+                dmin = Double.min(dmin, ardoubles[ii]);
+
+        return dmin;
+    }
+	
+    //-------------------------------------------------------------------------
+    /**
+     * Returns mean from an array of normally distributed values
+     * @param  ardoubles given array
+     * @return double - mean value (calculated as simple arithmetic mean)
+     */
+    public static double mean(double[] ardoubles) {
+        double dsum = 0.0;
+        for (int ii=0; ii<ardoubles.length; ii++)
+            dsum += ardoubles[ii];
+
+        return dsum / ardoubles.length;
+    }
+	
+    //-------------------------------------------------------------------------
+    /**
+     * Returns mean from an array of values with different 'weight' (e.g. probability)
+     * @param  ardoubles given array of values
+     * @param  arweights given array of weights
+     * @return double - mean value (calculated as weighted arithmetic mean)
+     */
+    public static double weighted_mean(double[] ardoubles, double[] arweights){
+        if (ardoubles.length != arweights.length)
+            throw new IndexOutOfBoundsException("Calling ScStatistics.weighted_mean - arrays of different length");
+        
+        double dsum = 0.0;
+        double dsumweights = 0.0;
+        for (int ii=0; ii<ardoubles.length; ii++) {
+            dsum += ardoubles[ii] * arweights[ii];
+            dsumweights += arweights[ii];
+        }
+
+        return dsum / dsumweights;
+    }
+    
+    //-------------------------------------------------------------------------
+    /**
+     * Returns standard deviation an array of normally distributed values (expected mean value is unknown)
+     * @param  ardoubles given array
+     * @return double - standard deviation (calculated as simple arithmetic mean)
+     */
+    public static double stdev(double[] ardoubles) {
+        double dmean = mean(ardoubles);
+        double dsum_squares = 0.0;
+        for (int ii=0; ii<ardoubles.length; ii++)
+            dsum_squares += ardoubles[ii]*ardoubles[ii];
+        
+        return Math.sqrt((dsum_squares - dmean*dmean * ardoubles.length) / (ardoubles.length - 1));
+    }
+    
+    //-------------------------------------------------------------------------
+    /**
+     * Returns standard deviation an array of normally distributed values (please note: expected 'mean' value is known)
+     * @param  ardoubles given array
+     * @param  dmean the expected value of stochastic variable which is represented by array values
+     * @return double - standard deviation (calculated as simple arithmetic mean)
+     */
+    public static double stdev_mn(double[] ardoubles, double dmean) {
+        double dsum_squares = 0.0;
+        for (int ii=0; ii<ardoubles.length; ii++)
+            dsum_squares += ardoubles[ii]*ardoubles[ii];
+        
+        return Math.sqrt((dsum_squares - dmean*dmean * ardoubles.length) / ardoubles.length);
+    }
+    
+    //-------------------------------------------------------------------------
+    /**
+     * Returns standard deviation from an array of values with different 'weight' (e.g. probability)
+     * @param  ardoubles given array
+     * @param  arweights given array of weights
+     * @return double - standard deviation (calculated as simple arithmetic mean)
+     */
+    public static double weighted_stdev(double[] ardoubles, double[] arweights) {
+        double dmean = weighted_mean(ardoubles, arweights);
+        double dsum_squares = 0.0;
+        double dsumweights = 0.0;
+        for (int ii=0; ii<ardoubles.length; ii++) {
+            dsum_squares += ardoubles[ii]*ardoubles[ii] * arweights[ii];
+            dsumweights += arweights[ii];
+        }
+        
+        return Math.sqrt((dsum_squares - dmean*dmean * dsumweights) / dsumweights);
+    }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Returns two sided quantile of Student's distribution with the 95% confidence interval
+     * @param  inum number of measured data in dataset
+     * @return double - quantile of Student's distribution
+     * @see https://en.wikipedia.org/wiki/Student%27s_t-distribution
+     * @todo confidence interval could be parameterized
+     */
+    static double get95StudentQuantil(int inum) {
+        if (inum < 1) 
+            throw new IllegalArgumentException("Calling ScStatistics.get95StudentQuantil - number of elements leass than 1");
+        
+        final double[] quantiles_1_30 = {
+        12.71, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365, 2.306, 2.262, 2.228, 
+        2.201, 2.179, 2.160, 2.145, 2.131, 2.120, 2.110, 2.101, 2.093, 2.086,
+        2.080, 2.074, 2.069, 2.064, 2.060, 2.056, 2.052, 2.048, 2.045, 2.042
+        }; 
+        
+        final double[] quantiles_32_50 = { // increment = 2
+        2.037, 2.032, 2.028, 2.024, 2.021, 2.018, 2.015, 2.013, 2.011, 2.009
+        };
+
+        final double[] quantiles_55_70 = { // increment = 5
+        2.004, 2.000, 1.997, 1.994 
+        };
+        
+        final double[] quantiles_80_100 = { // increment = 10
+        1.990, 1.987, 1.984 
+        };
+        
+        final double quantil_120 = 1.980;
+        
+        final double[] quantiles_150_300 = { // increment = 50
+        1.976, 1.972, 1.970, 1.968 
+        };
+        
+        final double[] quantiles_400_500 = { // increment = 100
+        1.966, 1.965 
+        };
+        
+        final double quantil_infinity = 1.960;
+        
+        if (inum < 30) return quantiles_1_30[inum-1];
+        else if (inum < 32) return quantiles_1_30[29]; // 2.042
+        else if (inum <= 50) return quantiles_32_50[(inum-32) / 2];
+        else if (inum < 55) return quantiles_32_50[9]; // 2.009
+        else if (inum <= 70) return quantiles_55_70[(inum-55) / 5];
+        else if (inum < 80) return quantiles_55_70[3]; // 1.994
+        else if (inum <= 100) return quantiles_80_100[(inum-80) / 10];
+        else if (inum < 120) return quantiles_80_100[2]; // 1.984
+        else if (inum < 150) return quantil_120; // 1.980
+        else if (inum <= 300) return quantiles_150_300[(inum-150) / 50];
+        else if (inum < 400) return quantiles_150_300[3]; // 1.968
+        else if (inum < 500) return quantiles_400_500[0]; // 1.966
+        else if (inum < 700) return quantiles_400_500[1]; // 1.965
+        else
+            return quantil_infinity; // 1.96
+    }
+    
+    //-------------------------------------------------------------------------
+    /**
+     * Equal mean values statistical test. Returns true if two mean values 
+     * can be consider stochastically equal (confidence interval - 95%).
+     * The assumption is that the both datasets are homogenous in the sense of 
+     * the measurement accuracy. (e.g. measured with the same sensor)
+     * @param  dmean1 first mean
+     * @param  dstdev1 standard deviation of measurements (values) in the first dataset
+     * @param  inum1 number of elements in the first dataset
+     * @param  dmean2 second  mean
+     * @param  dstdev2 standard deviation of measurements (values) in the second dataset
+     * @param  inum2 number of elements in the second dataset
+     * @return  boolean - true if means can be considered equal (in the stochastic sense)
+     */
+    public static boolean areMeansEqual(double dmean1, double dstdev1, int inum1, 
+                                        double dmean2, double dstdev2, int inum2) {
+        
+        final double quantil95 = get95StudentQuantil(Integer.min(inum1, inum2));
+        
+        double dstdev =  Math.sqrt(dstdev1*dstdev1 / inum1 + dstdev2*dstdev2 / inum2);
+        double ddiff = Math.abs(dmean1 - dmean2);
+        double dtest_value = ddiff / dstdev;
+        
+        return dtest_value <= quantil95;
+    }
+}	
---
+++ Steady_course.java
@@ -0,0 +1,179 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Steady_course {
+
+    //-------------------------------------------------------------------------
+    // internal utility data structure
+    public static class SCPair {
+        public SCPair (Tote _first, Tote _second) {
+            first = _first;
+            second = _second;
+        }
+        
+        public Tote first;
+        public Tote second;
+    }
+      
+    //-------------------------------------------------------------------------
+    // First try to extract steady-coarse periods
+    // It's a very rough determination (without any statistics). Just first try; just the beginning...
+    // (todo: min time interval as argument)
+    public static ArrayList<SCPair> steady_coarse_periods(ArrayList<Tote> totes, int minelements, double djump) {
+        
+        ArrayList<SCPair> periods = new ArrayList<>();
+        
+        Tote first = totes.get(0);
+        
+        Iterator<Tote> iter = totes.iterator();
+        iter.next(); // start iterating from the second element in array
+        while (iter.hasNext()) {
+            Tote this_tote = iter.next();
+            if (Math.abs(this_tote.dheading - first.dheading) > djump) {
+                if (this_tote.index - first.index >= minelements) periods.add(new SCPair(first, totes.get(this_tote.index-1)));
+
+                first = this_tote;
+            }
+        }
+
+        return periods;
+    }
+    
+    //-------------------------------------------------------------------------
+    // First try to extract steady-speed periods
+    // It's a very rough determination (rather useless and without any 
+    // statistics, at the moment). Just first try; just the beginning...
+    // (todo: min time interval as argument)
+    public static ArrayList<SCPair> steady_speed_periods(ArrayList<Tote> totes, int minelements, double djump) {
+        
+        ArrayList<SCPair> periods = new ArrayList<>();
+        
+        Tote first = totes.get(0);
+        
+        Iterator<Tote> iter = totes.iterator();
+        iter.next(); // start iterating from the second element in array
+        while (iter.hasNext()) {
+            Tote this_tote = iter.next();
+            if (Math.abs(this_tote.dspeed - first.dspeed) > djump) {
+                if (this_tote.index - first.index >= minelements) periods.add(new SCPair(first, totes.get(this_tote.index-1)));
+
+                first = this_tote;
+            }
+        }
+
+        return periods;
+    }
+
+    //-------------------------------------------------------------------------
+    // Test printing of totes. Elements between istart (inclusive)
+    // and iend (exclusive) will be printed.
+    static void print(ArrayList<Tote> totes, int istart, int iend) {
+        
+        // todo - validate istart and iend arguments
+        double delapsed_max = totes.get(iend-1).dabsolute_time - totes.get(0).dabsolute_time;
+        String str_format = delapsed_max < 10   ? "%3.1f" :
+                            delapsed_max < 100  ? "%4.1f" :
+                            delapsed_max < 1000 ? "%5.1f" :
+                            delapsed_max < 1000 ? "%6.1f" :
+                                                  "%7.1f" ;
+
+        for(int ii=istart; ii<iend; ii++) { 
+            Tote tote = totes.get(ii);
+            String str_space = tote.index < 10 ? "    " :
+                               tote.index < 100 ? "   " :
+                               tote.index < 1000 ? "  " :
+                               tote.index < 10000 ? " " :
+                                                     "" ;
+
+            double delapsed = tote.dabsolute_time - totes.get(0).dabsolute_time;
+
+            System.out.println(tote.index + ")" + str_space + "time: "    + String.format("%.2f", tote.dabsolute_time) + "  " +
+                                                              "elapsed: " + String.format(str_format, delapsed) + "  "  +
+                                                              "course: "  + String.format("%.2f", tote.dheading) + "  " +
+                                                              "speed: "   + String.format("%.2f", tote.dspeed) );
+        }
+    }
+
+    
+    //=========================================================================
+    public static void main(String[] args) {
+        
+    	try {
+            SCFileReader myreader = new SCFileReader();
+            ArrayList<Tote> totes = myreader.process("OtherOwnship_Trimmed.txt");
+
+            // printing test (only first 10 totes)
+            print(totes, 0, 10);
+            
+            // printing the first 3 (roughly determined) steady-corse periods
+            System.out.println();
+            ArrayList<SCPair> course_periods = steady_coarse_periods(totes, 10, 0.5);
+            for(int ii=0; ii<3; ii++) {
+                System.out.println("steady_coarse_period: index1=" + course_periods.get(ii).first.index + "  index2=" + course_periods.get(ii).second.index);
+                System.out.println("      in other words: time1=" + course_periods.get(ii).first.stime + "  time2=" + course_periods.get(ii).second.stime);
+            }
+            
+            // printing the first 3 (roughly determined) steady-speed periods
+            System.out.println();
+            ArrayList<SCPair> speed_periods = steady_speed_periods(totes, 10, 0.1);
+            for(int ii=0; ii<3; ii++) {
+                System.out.println("steady_coarse_period: index1=" + course_periods.get(ii).first.index + "  index2=" + course_periods.get(ii).second.index);
+                System.out.println("      in other words: time1=" + course_periods.get(ii).first.stime + "  time2=" + course_periods.get(ii).second.stime);
+            }
+    	}
+    	
+    	catch(Exception e) {
+            System.out.println(e);
+    	}
+    }
+}
+
+
+/*
+            // small trivial unit test
+            System.out.println();
+
+            double[] dspeeds = SCStatistics.getSpeeds(totes, 0, 10);
+            System.out.println("max speed = " + SCStatistics.max(dspeeds));
+            System.out.println("min speed = " + SCStatistics.min(dspeeds));
+            System.out.println("speed mean = " + SCStatistics.mean(dspeeds));
+            System.out.println("speed stdev = " + SCStatistics.stdev(dspeeds));
+
+            double[] weights0 = new double[dspeeds.length];
+            for (int jj=0; jj<weights0.length; jj++) weights0[jj]=1.0;
+            System.out.println("speed weighted_stdev  = " + SCStatistics.weighted_stdev(dspeeds, weights0));
+
+            System.out.println();
+
+            double[] dheadings = SCStatistics.getHeadings(totes, 0, 10);
+            System.out.println("max course = " + SCStatistics.max(dheadings));
+            System.out.println("min course = " + SCStatistics.min(dheadings));
+            System.out.println("course mean = " + SCStatistics.mean(dheadings));
+            System.out.println("course stdev = " + SCStatistics.stdev(dheadings));
+            System.out.println("speed weighted_stdev  = " + SCStatistics.weighted_stdev(dheadings, weights0));
+
+            System.out.println();
+
+            double[] dheadings26 = SCStatistics.getHeadings(totes, 2, 6);
+            double[] weights = new double[dheadings26.length];
+            weights[0] = 1.0; weights[1] = 0.5; weights[2] = 0.5; weights[3] = 1.0;
+            System.out.println("mean course2 = " + SCStatistics.mean(dheadings26));
+            System.out.println("weighte mean course2 = " + SCStatistics.weighted_mean(dheadings26, weights));
+            System.out.println("course2 stdev = " + SCStatistics.stdev(dheadings26));
+            System.out.println("course2 weighted stdev = " + SCStatistics.weighted_stdev(dheadings26, weights));
+
+            System.out.println();
+            
+            double[] dheadings2 = SCStatistics.getHeadings(totes, 4, 8);
+            double dmean2 = SCStatistics.min(dheadings2);
+            double dstdev2 = SCStatistics.stdev(dheadings2);
+
+            double[] dheadings3 = SCStatistics.getHeadings(totes, 22, 29);
+            double dmean3 = SCStatistics.min(dheadings3);
+            double dstdev3 = SCStatistics.stdev(dheadings3);
+            
+            boolean beq = SCStatistics.areMeansEqual(dmean3, dstdev3, dheadings3.length, dmean2, dstdev2, dheadings2.length);
+            System.out.println(beq ? "equal means" : "means are not equal");
+
+*/
