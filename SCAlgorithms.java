ec08de08607ce16306553d02e28ea2d55235e1a1
sasamil <sasa.milenkovic.xyz@gmail.com>
2018-07-08 14:06:44 +0200

    Added fifo_span_st()
    fifo_span_st(double[] input_array, int minelements)
    
    Returns intervals of steady courses/speeds. The statistic used for testing is: sqrt(2) * range / sigma < quantile(99%).
    Imo, interesting but it's still far from the desired goal.

--- SCAlgorithms.java
+++ SCAlgorithms.java
@@ -25,10 +25,10 @@ public class SCAlgorithms {
     
     //-------------------------------------------------------------------------
     /**
-     * Creates a double array of steady values (course or speed) values from an array of input values.
-     * @param  nput_array the array of input values
+     * Creates an array of steady values (course or speed) intervals from an array of input values.
+     * @param  input_array the array of input values
      * @param minelements the minimal number of elements in the 'steady' interval
-     * @param the maximal difference between the start value and end value
+     * @param maxspan the maximal difference between the start value and end value
      * @return double[] - array of speeds extracted form totes
      */
     public static ArrayList<SpanPair> fifo_span(double[] input_array, int minelements, double maxspan) {
@@ -47,6 +47,40 @@ public class SCAlgorithms {
             }
         }
                     
+        return periods;
+    }
+    
+    //-------------------------------------------------------------------------
+    /**
+     * Creates an array of steady values (course or speed) intervals from an 
+     * array of input values. Based on statistically determined maximal allowed span value.
+     * @param  input_array the array of input values
+     * @param minelements the minimal number of elements in the 'steady' interval
+     * @return double[] - array of speeds extracted form totes
+     */
+    public static ArrayList<SpanPair> fifo_span_st(double[] input_array, int minelements) {
+        
+        ArrayList<SpanPair> periods = new ArrayList<>();
+        
+        int istart=0, iend=0;
+        while (istart <= input_array.length - minelements && iend < input_array.length) {
+            boolean bcondition = false;
+            if (iend++ == istart) bcondition = Math.abs(input_array[iend] - input_array[istart]) < 2.5; // max jump, 10.0 for course, 2.5 for speed
+            else if (input_array.length == iend) bcondition = true;
+            else {
+                double dstdev = SCStatistics.stdev(input_array, istart, iend);
+                double drange = Math.abs(input_array[iend] - input_array[istart]);
+                bcondition = drange * Math.sqrt(2.0) / dstdev <= SCStatistics.get99StudentQuantil(iend - istart);
+            }
+            if(false == bcondition) {
+                if (iend - istart > minelements) {
+                    periods.add(new SpanPair(istart, iend));
+                    istart = iend;
+                }
+                else iend = ++istart;
+            }
+        }
+
         return periods;
     }
 }
