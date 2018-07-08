df1625582d7f6dd47382d228c1f40d6a09c9d0ad
sasamil <sasa.milenkovic.xyz@gmail.com>
2018-07-09 01:04:13 +0200

    Added fifo_mean_st()
    fifo_mean_st(double[] input_array, int minelements)
    
    The basic idea is to test deviation from the mean value in an interval and to compare its norm-value with quantile of Student's distribution (numelements-2 degrees of freedom).
    
    Imo, it's promising but it need time to test the method (possibly, with different datasets) and to brush up additionally.
    
    There are intervals which need to be merged. (The new function merge_intervals() will be created next)

--- SCAlgorithms.java
+++ SCAlgorithms.java
@@ -83,4 +83,52 @@ public class SCAlgorithms {
 
         return periods;
     }
+    //-------------------------------------------------------------------------
+    /**
+     * Creates an array of steady value (course or speed) intervals from an 
+     * array of input values. Based on statistics and calculated limits 
+     * for deviation from the mean value.
+     * @param  input_array the array of input values
+     * @param minelements the minimal number of elements in the 'steady' interval
+     * @return double[] - array of speeds extracted form totes
+     * todo it should be optimized. It can run significantly faster.
+     */
+    public static ArrayList<SpanPair> fifo_mean_st(double[] input_array, int minelements) {
+        
+        ArrayList<SpanPair> periods = new ArrayList<>();
+        
+        int istart=0, iend=1;
+        while ((istart <= input_array.length - minelements) && (iend < input_array.length)) {
+            boolean bcondition = false;
+            if (++iend < input_array.length) {
+                int numelements = iend - istart;
+                final double dmean = SCStatistics.mean(input_array, istart, iend);
+                final double dstdev = SCStatistics.stdev(input_array, istart, iend);
+                if (dstdev < 0.01) // Preventing devzerro errors
+                    bcondition = true;
+                else { // The 2nd test statistic is compared with the quantile of Student's S(0,1) distribution with inumelements-2 degrees of freedom
+                    final double dval = input_array[iend];
+                    final double ddeviation = Math.abs(dval - dmean);
+                    final double drange = Math.abs(input_array[iend] - input_array[istart]);
+                    final double dtest1 = drange * Math.sqrt(2.0) / dstdev;
+                    final double dtest2 = ddeviation / dstdev;
+                    final double dquantile1 = SCStatistics.get999StudentQuantil(numelements - 1);
+                    final double dquantile2 = numelements > 2 ? SCStatistics.get99StudentQuantil(numelements - 2) : dquantile1;
+                    bcondition = dtest1 <= dquantile1;
+                    bcondition &= dtest2 <= dquantile2;
+                }
+            }
+
+            if(false == bcondition) {
+                if (iend - istart > minelements) {
+                    periods.add(new SpanPair(istart, iend));
+                    istart = iend;
+                    iend = istart + 1;
+                }
+                else iend = ++istart + 1;
+            }
+        }
+
+        return periods;
+    }
 }
