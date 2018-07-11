697b99ce8a86bdd7256c734688de2c0a8fb7933f
sasamil <sasa.milenkovic.xyz@gmail.com>
2018-07-11 10:13:23 +0200

    Update SCAlgorithms.java
    Added fifo_mean_st_maxdev()
    
    The maximal deviation within treated subarray is compared to test statistics. It makes fifo_mean_st more strict but more accurate.

--- SCAlgorithms.java
+++ SCAlgorithms.java
@@ -131,4 +131,57 @@ public class SCAlgorithms {
 
         return periods;
     }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Creates an array of steady value (course or speed) intervals from an 
+     * array of input values. Based on statistics and calculated limits 
+     * for deviation from the mean value. The maximal deviation is compared.
+     * @param  input_array the array of input values
+     * @param minelements the minimal number of elements in the 'steady' interval
+     * @return double[] - array of speeds extracted form totes
+     * todo it should be optimized. It can run significantly faster.
+     */
+    public static ArrayList<SpanPair> fifo_mean_st_maxdev(double[] input_array, int minelements) {
+        
+        ArrayList<SpanPair> periods = new ArrayList<>();
+        
+        int istart=0, iend=1;
+        while ((istart <= input_array.length - minelements) && (iend < input_array.length)) {
+            boolean bcondition = false;
+            if (++iend < input_array.length) {
+                int numelements = iend - istart;
+                final double dmax = SCStatistics.max(input_array, istart, iend);
+                final double dmin = SCStatistics.min(input_array, istart, iend);
+                if (dmax - dmin < 0.01)
+                    bcondition = true;
+                else { // The 2nd test statistic is compared to the quantile of Student's S(0,1) distribution with inumelements-2 degrees of freedom
+                    final double dstdev = SCStatistics.stdev(input_array, istart, iend);
+                    if (dstdev < 0.01) // Preventing devzerro errors
+                        bcondition = true;
+                    else {
+                        final double dmean = SCStatistics.mean(input_array, istart, iend);
+                        final double ddeviation = Math.max(dmax - dmean, dmean - dmin);
+                        final double drange = Math.abs(input_array[iend-1] - input_array[istart]);
+                        final double dtest1 = drange * Math.sqrt(2.0) / dstdev;
+                        final double dtest2 = ddeviation / dstdev;
+                        final double dquantile1 = SCStatistics.get99StudentQuantil(numelements - 1);
+                        final double dquantile2 = numelements > 2 ? SCStatistics.get999StudentQuantil(numelements - 2) : dquantile1;
+                        bcondition = dtest1 <= dquantile1 && dtest2 <= dquantile2;
+                    }
+                }
+            }
+
+            if(false == bcondition) {
+                if (iend - istart > minelements) {
+                    periods.add(new SpanPair(istart, iend));
+                    istart = iend;
+                    iend = istart + 1;
+                }
+                else iend = ++istart + 1;
+            }
+        }
+
+        return periods;
+    }
 }
